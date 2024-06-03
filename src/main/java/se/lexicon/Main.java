7465c38fabb593cebce4feb22da87305e0c51e00
Sami Alabed <sami.alabed.94@gmail.com>
2024-06-03 15:18:58 +0200

    Create a project, and first exercise.

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,11 @@
+package se.lexicon;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Exercise 1.
+        System.out.println("Hello\nSami!");
+    }
+}
